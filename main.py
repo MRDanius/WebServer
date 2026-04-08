55e7bc6dd6fbded91eb07e445cbed197ecec7947
Ryx1Prime <gadole2019@gmail.com>
2026-04-08 18:00:59 +0500

    configuration loading is implemented with support for default values

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    pass
+from server.config import Config
 
-
+def test_config():
+    print("--------Пошёл запуск теста моего конфига-------")
+    cfg = Config()
+    print(f"Host {cfg.host}")
+    print(f"Port {cfg.port}")
+    print(f"Root DIr {cfg.root_dir}")
+    print(f"Log file {cfg.log_file}")
 if __name__ == "__main__":
-    main()
+    test_config()
--- server/config.py
+++ server/config.py
@@ -1,2 +1,31 @@
+import json
+
+
 class Config:
-    pass
+    def __init__(self, file_path='config.json'):
+        # добавлю значения по умолчанию вдруг с файлом конфига что то пойдёт не так, чтобы мы по итогу всё равно смогли
+        # запустить наш сервер
+        self.host = "127.0.0.1"
+        self.port = 8080
+        self.root_dir = "./static"
+        self.log_file = "webserver.log"
+        self.ssl_cert = ""
+        self.ssl_key = ""
+        try:
+            with open(file_path, 'r', encoding='utf-8') as config:
+                data = json.load(config)
+
+            self.host = data['host_ip']
+            self.port = data['port']
+            self.root_dir = data['root_dir']
+            self.log_file = data['log_file']
+            self.ssl_cert = data['ssl_cert']
+            self.ssl_key = data['ssl_key']
+
+            print("OK! Конфиг успешно загружен из файла")
+        except FileNotFoundError:
+            print(f"[ERROR]: Файл не конфигурации не найден, будут использованы значения по умолчанию ")
+        except json.JSONDecodeError:
+            print(f"[ERROR]: Ошибка в файле конфигурации, проверьте JSON, будут использованы значения по умолчанию")
+        except Exception as e:
+            print(f"[ERROR]: Непредвиденная ошибка! {e}")
